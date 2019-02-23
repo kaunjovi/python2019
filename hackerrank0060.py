fc3dee205d6fe337a82c70d4a5fe6af74a7c4a4e
kaunjovi <kaunjovi@gmail.com>
2019-02-23 21:08:24 +0530

    The initial print was also not too good. Need to import __future__

---
+++ hackerrank0060.py
@@ -0,0 +1,10 @@
+
+# https://www.hackerrank.com/challenges/python-print/tutorial
+
+from __future__ import print_function
+
+number = int(input())
+
+
+for i in range(number) : 
+  print(i+1, sep='', end='')
